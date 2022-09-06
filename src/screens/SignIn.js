3694add845fe942276b591d57f0754961e91d0c1
ARTHUR ALVES CALDEIRA <0057842@IFMG.EDU.BR>
2022-09-06 11:45:41 -0300

    Inicio da tela de login

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Text ,View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function SignIn() {
  return (
-   <View />
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/img/login.png')}
+        resizeMode='contain'
+        style={{ width: '75%'}}
+      />
+    </View>
   );
-}
+ }
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#6E9987',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+  });
