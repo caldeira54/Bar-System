b60a496bbccd4a8ac583545b1617cd5bea7405f7
ARTHUR ALVES CALDEIRA <0057842@IFMG.EDU.BR>
2022-09-13 10:57:42 -0300

    atualizacao da tela de login

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Text ,View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import colors from '../global/colors.js';
 
@@ -8,85 +8,86 @@ export default function SignIn() {
 
   const navigation = useNavigation();
 
-  function handleSignIn(){
-      navigation.navigate("home");
+  function handleSignIn() {
+    navigation.navigate("home");
   }
 
- return (
-    <View style={styles.container}>
-      
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
 
-      <View style={styles.form}>
-        <Image
-          source={require('../assets/img/sinuca.png')}
-          resizeMode='contain'
-          style={{ width: '75%', height: '30%', marginLeft: '13%' }}
-          
-        />
-        
-        <Text style={styles.label}>Usuario:</Text>
+        <View style={styles.form}>
+          <Image
+            source={require('../assets/img/sinuca.png')}
+            resizeMode='contain'
+            style={{ width: '75%', height: '30%', marginLeft: '13%' }}
 
-        <TextInput
-          style={styles.input}
-        />
+          />
 
-        <Text style={styles.label}>Senha:</Text>   
+          <Text style={styles.label}>Usuario:</Text>
 
-        <TextInput
-          style={styles.input}
-        />
+          <TextInput
+            style={styles.input}
+          />
 
-        <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-          <Text style={styles.textButton}>
+          <Text style={styles.label}>Senha:</Text>
+
+          <TextInput
+            style={styles.input}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+            <Text style={styles.textButton}>
               Entrar
-          </Text>
-        </TouchableOpacity>
+            </Text>
+          </TouchableOpacity>
 
         </View>
-    </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
- }
-  const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#6E9987',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    label: {
-      fontSize: 30,
-      color: colors('cinzaclaro'),
-      marginBottom: 8
-    },
-    button: {
-      backgroundColor: colors('verdeescuro'),
-      padding: 4,
-      height: 40,
-      width: '100%',
-      textAlign: 'center',
-      borderRadius: 12,
-      marginTop: 20
-    },
-    textButton: {
-      fontSize: 16,
-      padding: 4,
-      textTransform: 'uppercase',
-      textAlign: 'center',
-      color: colors('branco')
-    },
-    form: {
-      width: '75%',
-      flex: 0.8,
-      justifyContent: 'center'
-    },
-    input: {
-      height: 40,
-      backgroundColor: colors('cinzaclaro'),
-      borderRadius: 12,
-      borderBottomWidth: 1,
-      color:'#041122',
-      padding: 4,
-      fontSize: 16,
-      marginBottom: 30
-    }
-  });
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6E9987',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  label: {
+    fontSize: 30,
+    color: colors('cinzaclaro'),
+    marginBottom: 8
+  },
+  button: {
+    backgroundColor: colors('verdeescuro'),
+    padding: 4,
+    height: 40,
+    width: '100%',
+    textAlign: 'center',
+    borderRadius: 12,
+    marginTop: 20
+  },
+  textButton: {
+    fontSize: 16,
+    padding: 4,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    color: colors('branco')
+  },
+  form: {
+    width: '75%',
+    flex: 0.8,
+    justifyContent: 'center'
+  },
+  input: {
+    height: 40,
+    backgroundColor: colors('cinzaclaro'),
+    borderRadius: 12,
+    borderBottomWidth: 1,
+    color: '#041122',
+    padding: 4,
+    fontSize: 16,
+    marginBottom: 30
+  }
+});
